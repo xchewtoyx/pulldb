deba63ff3bb8cca70a9cc5bb682d4d455eca0ae8
Russell Heilling <russell@heilling.net>
2013-09-19 11:11:03 +0100

    Create a site-packages directory that can be used to hold unmodified source of external modules.
    Add python-dateutil to the site packages directory.
    
    Move pycomicvine submodule to the site-packages.

--- pulldb/__init__.py
+++ pulldb/__init__.py
@@ -1,4 +1,5 @@
 import os
+import site
 
 from google.appengine.api import users
 
@@ -6,6 +7,9 @@ import jinja2
 import webapp2
 
 from pulldb import index
+from pulldb.util import AppRoot
+
+site.addsitedir(os.path.join(AppRoot(), 'site-packages'))
 
 application = webapp2.WSGIApplication([
     ('/', index.MainPage),
