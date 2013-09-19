c7786afd27c4b01aa096de23d381597f7e23b397
Russell Heilling <russell@heilling.net>
2013-09-19 16:12:04 +0100

    Add skeleton for users handlers

--- pulldb/users.py
+++ pulldb/users.py
@@ -2,9 +2,34 @@
 
 from google.appengine.ext import ndb
 
+from pulldb import base
+from pulldb import session
+
+import webapp2 
+
 class User(ndb.Model):
   '''User object in datastore.
 
   Holds the email and ID of the users that have a pull-list.
   '''
+  userid = ndb.IntegerProperty()
+  image = ndb.StringProperty()
   email = ndb.StringProperty()
+  oauth_token = ndb.StringProperty()
+
+class Profile(session.SessionHandler):
+  def get(self):
+    template_values = self.base_template_values()
+    template_values.update({
+    })
+    template = self.templates.get_template('users_profile.html')
+    self.response.write(template.render(template_values))
+
+class Register(base.BaseHandler):
+  def post(self):
+    self.redirect('/users')
+
+app = webapp2.WSGIApplication([
+    ('/users/me', Profile),
+    ('/users/register', Register),
+], debug=True)
