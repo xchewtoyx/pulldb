ea14bf75aaf07b20cd66321e52d411f8b6f3e165
Russell Heilling <russell@heilling.net>
2013-09-19 17:44:53 +0100

    Save user entry to datastore if not present

--- pulldb/users.py
+++ pulldb/users.py
@@ -1,5 +1,6 @@
 # Copyright 2013 Russell Heilling
 
+from google.appengine.api import users
 from google.appengine.ext import ndb
 
 from pulldb import base
@@ -12,24 +13,25 @@ class User(ndb.Model):
 
   Holds the email and ID of the users that have a pull-list.
   '''
-  userid = ndb.IntegerProperty()
+  userid = ndb.StringProperty()
   image = ndb.StringProperty()
-  email = ndb.StringProperty()
+  nickname = ndb.StringProperty()
   oauth_token = ndb.StringProperty()
 
 class Profile(session.SessionHandler):
   def get(self):
+    user = users.get_current_user()
+    user_key = User.query(User.userid == user.user_id())
+    if not user_key:
+      user_key = User(userid=user.user_id(), nickname=user.nickname())
+      user_key.put()
     template_values = self.base_template_values()
     template_values.update({
+        'user': user_key,
     })
     template = self.templates.get_template('users_profile.html')
     self.response.write(template.render(template_values))
 
-class Register(base.BaseHandler):
-  def post(self):
-    self.redirect('/users')
-
 app = webapp2.WSGIApplication([
     ('/users/me', Profile),
-    ('/users/register', Register),
 ], debug=True)
