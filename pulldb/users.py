a82709acadef1fa7991f733df20bd54252172e01
Russell Heilling <russell@heilling.net>
2013-09-19 20:54:46 +0100

    Update user addition code and add welcome message.

--- pulldb/users.py
+++ pulldb/users.py
@@ -1,10 +1,12 @@
 # Copyright 2013 Russell Heilling
+import logging
 
 from google.appengine.api import users
 from google.appengine.ext import ndb
 
 from pulldb import base
 from pulldb import session
+from pulldb.publishers import Publisher
 
 import webapp2 
 
@@ -17,12 +19,14 @@ class User(ndb.Model):
   image = ndb.StringProperty()
   nickname = ndb.StringProperty()
   oauth_token = ndb.StringProperty()
+  publisher = ndb.KeyProperty(key=Publisher)
 
 class Profile(session.SessionHandler):
   def get(self):
     user = users.get_current_user()
-    user_key = User.query(User.userid == user.user_id())
+    user_key = User.query(User.userid == user.user_id()).get()
     if not user_key:
+      logging.info('Adding user to datastore: %s', user.nickname())
       user_key = User(userid=user.user_id(), nickname=user.nickname())
       user_key.put()
     template_values = self.base_template_values()
