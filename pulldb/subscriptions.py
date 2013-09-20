88995b5e2bcd6f3e21d8810a97f3c38cc84e8189
Russell Heilling <russell@heilling.net>
2013-09-20 10:22:19 +0100

    Add basic subscription fetcher / creator

--- pulldb/subscriptions.py
+++ pulldb/subscriptions.py
@@ -2,6 +2,8 @@
 
 from google.appengine.ext import ndb
 
+from pulldb.users import user_key
+
 class Subscription(ndb.Model):
   '''Subscription object in datastore.
 
@@ -9,3 +11,19 @@ class Subscription(ndb.Model):
   '''
   start_date = ndb.DateProperty()
   volume = ndb.KeyProperty(kind='Volume')
+
+def subscription_key(volume_key, create=False):
+  key = None
+  user = user_key()
+  subscription = Subscription.query(Subscription.volume==volume_key,
+                                    ancestor=user).get()
+  if subscription:
+    key = subscription.key
+  elif create:
+    subscription = Subscription(parent=user, 
+                                volume=volume_key)
+    subscription.put()
+    key = user.key
+  return key
+
+  
