68d04978bdd8187a90475284f603317ab5b61f5d
Russell Heilling <russell@heilling.net>
2013-09-20 11:01:18 +0100

    Add basic subscription listing support

--- pulldb/subscriptions.py
+++ pulldb/subscriptions.py
@@ -2,7 +2,8 @@
 
 from google.appengine.ext import ndb
 
-from pulldb.users import user_key
+from pulldb import base
+from pulldb import users
 
 class Subscription(ndb.Model):
   '''Subscription object in datastore.
@@ -14,7 +15,7 @@ class Subscription(ndb.Model):
 
 def subscription_key(volume_key, create=False):
   key = None
-  user = user_key()
+  user = users.user_key()
   subscription = Subscription.query(Subscription.volume==volume_key,
                                     ancestor=user).get()
   if subscription:
@@ -26,4 +27,42 @@ def subscription_key(volume_key, create=False):
     key = user.key
   return key
 
-  
+class MainPage(base.BaseHandler):
+  def get(self):
+    def subscription_detail(subscription):
+      volume = subscription.volume.get()
+      return {
+        'volume': volume,
+        'publisher':  volume.publisher.get(),
+        'subscription': subscription,
+      }
+      
+    results = Subscription.query(ancestor=users.user_key())
+    template_values = self.base_template_values()
+    template_values.update({
+        'results': (
+          subscription_detail(subscription) for subscription in results),
+        'results_count': results.count(),
+    })
+    template = self.templates.get_template('subscriptions_list.html')
+    self.response.write(template.render(template_values))
+
+
+class AddSub(base.BaseHandler):
+  def post(self):
+    pass
+
+class RemoveSub(base.BaseHandler):
+  def post(self):
+    pass
+
+class UpdateSub(base.BaseHandler):
+  def post(self):
+    pass
+
+app = base.create_app([
+    ('/subscriptions', MainPage),
+    ('/subscriptions/add', AddSub),
+    ('/subscriptions/remove', RemoveSub),
+    ('/subscriptions/update', UpdateSub),
+])  
