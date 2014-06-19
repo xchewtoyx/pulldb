d20665af1c4cf5b0b9356fc3c1e07854debc08ba
Russell Heilling <russell@linx.net>
2014-06-19 22:40:21 +0100

    Remove code that has been migrated to alternate modules

--- pulldb/models/properties.py
+++
@@ -1,6 +0,0 @@
-from google.appengine.ext import ndb
-
-class ImageProperty(ndb.StringProperty):
-    def _from_base_type(self, value):
-        if value.startswith('/'):
-            return 'http://static.comicvine.com' + value
--- pulldb/publishers.py
+++
@@ -1,3 +0,0 @@
-# Copyright 2013 Russell Heilling
-
-from pulldb.models.publishers import Publisher
